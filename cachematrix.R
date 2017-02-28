56e482f7f157a8a8888fd015ee5494b307cf918c
James Tseng <james.tseng00@gmail.com>
2017-02-28 21:48:45 +1100

    Updated with comments

--- cachematrix.R
+++ cachematrix.R
@@ -5,15 +5,15 @@
 
 makeCacheMatrix <- function(x = matrix())
 {
-  return_matrix <- NULL
+  return_matrix <- NULL #resets return_matrix
   set <- function(matrix_store)
   {
-    x <<- matrix_store 
-    return_matrix <<- NULL            
+    x <<- matrix_store #caches matrix_store to x
+    return_matrix <<- NULL #resets return_matrix          
   }
   get <- function() x
-  set_inverse <- function(solve) return_matrix <<- solve
-  get_inverse <- function() return_matrix
+  set_inverse <- function(solve) return_matrix <<- solve #set set_inverse as function which caches solve into return_matrix 
+  get_inverse <- function() return_matrix 
   list(set = set, get = get, 
        set_inverse = set_inverse, 
        get_inverse = get_inverse)
@@ -25,8 +25,8 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   
   matrix_store <- matrix_to_check$get_inverse()
-  if(!is.null(matrix_store))
-  {
+  if(!is.null(matrix_store)) #checks if matrix store cache exist
+  {#if doesn't, get cache
     message("Getting Cached Matrix")
     return(matrix_store)
   }
